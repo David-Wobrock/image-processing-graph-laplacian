52cd6d31e6073b24c0d322a9f34aca8342f87dac
David Wobrock <david.wobrock@gmail.com>
2018-02-15 11:49:13 +0100

    Try a row wise Gram Schmidt implementation

--- hpc/gram_schmidt.c
+++ hpc/gram_schmidt.c
@@ -8,7 +8,7 @@ Projection operator: proj_u(v) = (<u, v> / <u, u>) * u
 Input: v, u vectors, res should have been created
 Output: an allocated and filled res
 */
-static void Projection(const Vec v, const Vec u, Vec* res)
+/*static void Projection(const Vec v, const Vec u, Vec* res)
 {
     PetscScalar val1, val2, factor;
     VecDot(v, u, &val1);
@@ -18,6 +18,23 @@ static void Projection(const Vec v, const Vec u, Vec* res)
     VecCopy(u, *res);
 
     VecScale(*res, factor);
+}*/
+
+static double LocalVecDot(Vec x, Vec y)
+{
+    PetscInt istart, iend;
+    VecGetOwnershipRange(x, &istart, &iend);
+
+    double dot = 0;
+    double val_x, val_y;
+    for (PetscInt i = istart; i < iend; ++i)
+    {
+        VecGetValues(x, 1, &i, &val_x);
+        VecGetValues(y, 1, &i, &val_y);
+        dot += (val_x * val_y);
+    }
+
+    return dot;
 }
 
 /*
@@ -28,30 +45,49 @@ Output: Orthonormal X and norms filled with the norms of X before normalisation
 */
 void OrthonormaliseVecs(Vec* X, const unsigned int n, const unsigned int p, PetscScalar* norms)
 {
-    Vec sum_vec, proj_vec;
-
-    VecCreate(PETSC_COMM_WORLD, &sum_vec);
-    VecSetSizes(sum_vec, PETSC_DECIDE, n);
-    VecSetFromOptions(sum_vec);
-    VecAssemblyBegin(sum_vec);
-    VecAssemblyEnd(sum_vec);
-
-    VecDuplicate(sum_vec, &proj_vec);
-    VecSetFromOptions(proj_vec);
-    VecAssemblyBegin(proj_vec);
-    VecAssemblyEnd(proj_vec);
+    Vec X_j_copy;
+    VecCreate(PETSC_COMM_WORLD, &X_j_copy);
+    VecSetSizes(X_j_copy, PETSC_DECIDE, n);
+    VecSetFromOptions(X_j_copy);
+    VecAssemblyBegin(X_j_copy);
+    VecAssemblyEnd(X_j_copy);
 
     PetscScalar* current_norm = NULL;
-    for (unsigned int k = 0; k < p; ++k)
+    unsigned int k, j;
+    double factor;
+    double *dot_product_sums, *comm_sums;
+    dot_product_sums = (double*) malloc(sizeof(double) * p * 2);
+    comm_sums = (double*) malloc(sizeof(double) * p * 2);
+    for (k = 0; k < p; ++k)
     {
-        VecSet(sum_vec, 0);
-        for (unsigned int j = 0; j < k; ++j)
+        // Local sum or projections
+        for (j = 0; j < k; ++j)
         {
-            Projection(X[k], X[j], &proj_vec);
-            VecAXPY(sum_vec, 1., proj_vec);
+            dot_product_sums[j*2] = LocalVecDot(X[j], X[k]);
+            dot_product_sums[(j*2)+1] = LocalVecDot(X[j], X[j]);
         }
-        VecAXPBY(X[k], -1., 1, sum_vec);  // u_k = 1*v_k - sum
 
+        // AllReduce sums
+        MPI_Allreduce(
+            dot_product_sums,
+            comm_sums,
+            k*2,
+            MPI_DOUBLE,
+            MPI_SUM,
+            PETSC_COMM_WORLD);
+
+        // Apply orthogonalisation locally
+        for (j = 0; j < k; ++j)
+        {
+            // X_k = X_k - (a/b) * X_j
+            VecCopy(X[j], X_j_copy);
+            factor = ((double) comm_sums[j*2]) / (comm_sums[(j*2)+1]);
+            VecScale(X_j_copy, factor); // Not collective
+
+            VecAXPY(X[k], -1.0, X_j_copy);
+        }
+
+        // Normalise
         if (norms)
         {
             current_norm = norms+k;
@@ -59,8 +95,9 @@ void OrthonormaliseVecs(Vec* X, const unsigned int n, const unsigned int p, Pets
         VecNormalize(X[k], current_norm);
     }
 
-    VecDestroy(&proj_vec);
-    VecDestroy(&sum_vec);
+    free(comm_sums);
+    free(dot_product_sums);
+    VecDestroy(&X_j_copy);
 }
 
 void NormaliseVecs(Vec* X, const unsigned int p, PetscScalar* norms)
